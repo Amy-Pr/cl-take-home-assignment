e813f46d41d807ae8d6cb232e990361780304ec4
amy_pr <ampreci@gmail.com>
2023-06-09 09:34:04 -0700

    add onKeyDown event and role accessibility attribute to list element

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,40 +3,58 @@ import { useState } from 'react';
 import { searchArtworks } from '../api';
 import { SearchForm } from './SearchForm';
 import { Footer } from './Footer';
+import { ImageDetailsPage } from './ImageDetailsPage';
 
 import './App.css';
 
 export function App() {
 	const [data, setData] = useState([]);
+	const [selectedArtwork, setSelectedArtwork] = useState('');
 
 	function onSearchSubmit(query) {
-		// Search for the users's query.
-		// TODO: render the results, instead of logging them to the console.
-		// NOTE: `searchArtworks` currently returns local data, so that we
-		// don't make too many requests to the API! Once we've built out
-		// our UI, we need to make real requests!
-		// @see: ./src/api.js
+		setSelectedArtwork(null); // reset selected artwork
 		searchArtworks(query).then((json) => {
 			// console.log(json.data);
 			setData(json.data);
 		});
 	}
 
+	// function onBackClick() {
+	// 	setSelectedArtwork(null);
+	// }
+
 	return (
 		<div className="App">
 			<h1>TCL Career Lab Art Finder</h1>
-			<>
-				<SearchForm onSearchSubmit={onSearchSubmit} />
-				<ul>
-					{data.map(({ artist_title, id, title }) => (
-						<li key={id}>
-							<h3>
-								{title}, by {artist_title}
-							</h3>
-						</li>
-					))}
-				</ul>
-			</>
+
+			{selectedArtwork ? (
+				<ImageDetailsPage
+					artwork={data.find((artwork) => artwork.image_id === selectedArtwork)}
+					// onBackClick = {onBackClick}
+				/>
+			) : (
+				<>
+					<SearchForm onSearchSubmit={onSearchSubmit} />a
+					<ul>
+						{data.map(({ image_id, artist_title, title }) => (
+							<li
+								key={image_id}
+								role="none"
+								onClick={() => setSelectedArtwork(image_id)}
+								onKeyDown={(event) => {
+									if (event.key === 'Enter') {
+										setSelectedArtwork(image_id);
+									}
+								}}
+							>
+								<h3>
+									{title}, by {artist_title}
+								</h3>
+							</li>
+						))}
+					</ul>
+				</>
+			)}
 			<Footer />
 		</div>
 	);
